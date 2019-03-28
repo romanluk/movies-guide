2943abe55bbb4fd3baff99933b65a16b74fb5a4e
Roman Lukomskyi <rlukomskyi@gmail.com>
2019-03-28 19:18:51 +0200

    Customize navigation menu

--- app/src/main/java/com/example/moviesguide/MainActivity.kt
+++ app/src/main/java/com/example/moviesguide/MainActivity.kt
@@ -9,16 +9,16 @@ class MainActivity : AppCompatActivity() {
 
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
-            R.id.navigation_home -> {
-                message.setText(R.string.title_home)
+            R.id.navigation_trending -> {
+                message.setText(R.string.title_trending)
                 return@OnNavigationItemSelectedListener true
             }
-            R.id.navigation_dashboard -> {
-                message.setText(R.string.title_dashboard)
+            R.id.navigation_liked -> {
+                message.setText(R.string.title_liked)
                 return@OnNavigationItemSelectedListener true
             }
-            R.id.navigation_notifications -> {
-                message.setText(R.string.title_notifications)
+            R.id.navigation_search -> {
+                message.setText(R.string.title_search)
                 return@OnNavigationItemSelectedListener true
             }
         }
