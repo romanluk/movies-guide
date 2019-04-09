de05b8e2e1c2115f95b4cb98354c52a6bfdb481f
Roman Lukomskyi <rlukomskyi@gmail.com>
2019-04-09 21:45:47 +0300

    Fix back button issues

--- app/src/main/java/com/example/moviesguide/MainActivity.kt
+++ app/src/main/java/com/example/moviesguide/MainActivity.kt
@@ -51,7 +51,6 @@ class MainActivity : AppCompatActivity(), TrendingMoviesFragment.OnListFragmentI
     private fun openFragment(fragment: Fragment) {
         val transaction = supportFragmentManager.beginTransaction()
         transaction.replace(R.id.container, fragment)
-        transaction.addToBackStack(null)
         transaction.commit()
     }
 }
